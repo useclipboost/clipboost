1d3428f3e8a458c9e615adc52bef6499230fb68d
useclipboost <ryeenyon@gmail.com>
2026-05-25 10:03:25 -0400

    Add files via upload

---
+++ clipboost!/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ClipBoost from './ClipBoost.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClipBoost />
+  </React.StrictMode>
+)
