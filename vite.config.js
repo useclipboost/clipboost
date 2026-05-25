5802c07648b5eb8e07e159693fb97e45cc19de9d
useclipboost <ryeenyon@gmail.com>
2026-05-25 11:18:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-viteapp'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
