1a1e3d483bd5b005fbff2732a7e956e8d7af0e57
useclipboost <ryeenyon@gmail.com>
2026-05-25 10:14:37 -0400

    Delete clipboost! directory

--- clipboost!/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ClipBoost from './ClipBoost.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ClipBoost />
-  </React.StrictMode>
-)
