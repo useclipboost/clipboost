372d103a68a74fdc3153ba413ec4a6e690de4174
useclipboost <ryeenyon@gmail.com>
2026-05-26 15:44:30 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ClipBoost from './ClipBoost.jsx'
-import './index.css' // Ensures your default tailwind rules load correctly
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
