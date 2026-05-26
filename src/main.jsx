0cacc3a0df22c8467851eff6b33a31e7bf64cab5
useclipboost <ryeenyon@gmail.com>
2026-05-26 17:03:03 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ClipBoost from './ClipBoost.jsx'
+import ClipBoost from './ClipBoost.jsx' // Links directly to the file next to it
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
