fa020c24d659f6e700be1fdcfa1f1b629899dbb3
useclipboost <ryeenyon@gmail.com>
2026-05-26 16:09:31 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Saga from './Saga.jsx'
+import ClipBoost from './ClipBoost.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Saga />
+    <ClipBoost />
   </React.StrictMode>,
 )
